d264d2bed9c0cef109236dfd2204c924a2e88dc8
Esteban Rangel <erangel@anl.gov>
2021-09-18 00:52:43 +0000

    Minor update

---
+++ README.md
@@ -0,0 +1,13 @@
+
+'''
+module use /soft/modulefiles
+module load cmake gcc/6.5.0 cuda openmpi
+
+mkdir build
+cd build
+
+export KOKKOS_INSTALL=/gpfs/jlse-fs0/users/erangel/HACCabana/kokkos/build/install
+export CABANA_INSTALL=/gpfs/jlse-fs0/users/erangel/HACCabana/Cabana/build/install
+
+cmake -DCMAKE_PREFIX_PATH=$CABANA_INSTALL -DCMAKE_CXX_COMPILER=$KOKKOS_INSTALL/bin/nvcc_wrapper -DENABLE_GPU=ON ..
+'''
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 # example run using real data taken at step 7
-OMP_PROC_BIND=false ./build/driver_short-range ../PRE_rank0_step7_particle_data.bin ../POST_rank0_step7_particle_data.bin 7
+#OMP_PROC_BIND=false ./build/driver_short-range -i ../PRE_rank0_step7_particle_data.bin -v ../POST_rank0_step7_particle_data.bin -t 7
+OMP_PROC_BIND=false ./build/driver_short-range -i ../PRE_rank0_particle_data.bin -v ../POST_rank0_particle_data.bin -t 0
+#OMP_PROC_BIND=false ./build/driver_short-range -s -t 0
