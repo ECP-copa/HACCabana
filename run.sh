36c255f2b01896c4dbc77e103c2cfaae4343758d
Esteban Rangel <erangel@anl.gov>
2021-09-28 15:53:12 +0000

    Update to example run script.

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 # example run using real data taken at step 7
-#OMP_PROC_BIND=false ./build/driver_short-range -i ../PRE_rank0_step7_particle_data.bin -v ../POST_rank0_step7_particle_data.bin -t 7
-OMP_PROC_BIND=false ./build/driver_short-range -i ../PRE_rank0_particle_data.bin -v ../POST_rank0_particle_data.bin -t 0
-#OMP_PROC_BIND=false ./build/driver_short-range -s -t 0
+OMP_PROC_BIND=false ./build/driver_short-range -i ../1-rank-data/PRE_rank0_step7_particle_data.bin -v ../1-rank-data/POST_rank0_step7_particle_data.bin -t 7 -c 256.indat.params
+#OMP_PROC_BIND=false ./build/driver_short-range -s -t 0 -c 256.indat.params
