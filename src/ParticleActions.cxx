788b9ce012924153bbe5cf5a634cce5aa355c741
Sam Reeve <6740307+streeve@users.noreply.github.com>
2021-10-07 11:06:37 -0500

    Use Kokkos host/device sqrt

--- src/ParticleActions.cxx
+++ src/ParticleActions.cxx
@@ -122,7 +122,7 @@ void ParticleActions::updateVel(\
               if (dist2 < rmax2) 
               {
                 const float dist2Err = dist2 + rsm2;
-                const float tmp =  rsqrtf(dist2Err*dist2Err*dist2Err) - FGridEvalPoly(dist2);
+                const float tmp =  1/Kokkos::Experimental::sqrt(dist2Err*dist2Err*dist2Err) - FGridEvalPoly(dist2);
                 force[0] += dx * tmp;
                 force[1] += dy * tmp;
                 force[2] += dz * tmp;
